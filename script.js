caf2c14832e67753cddddb10a4f67ddf66d4dac9
bogdan-lun-02 <bogdanlun36@gmail.com>
2024-09-28 17:24:20 +0300

    Game complete

--- script.js
+++ script.js
@@ -1,83 +1,86 @@
-let game = {
-  gameboard: [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
-  ],
-  // logBoard: function () {
-  //   console.log(this.gameboard.forEach(cell => console.log(cell))
-  //   );
-  // },
+// Фабричные функции
 
+function createPlayer(name, symbol, id) {
+
+  let score = 0;
+
+  const makeTurn = (x) => {
+    if (game.gameboard[x] !== '') {
+      gameControl.switchActivePlayer();
+      return;
+    } else game.gameboard[x] = symbol;
+    gameControl.currentChoice = x;
+    id === 1 ? gameControl.player1Choices += x :
+      gameControl.player2Choices += x
+  }
+  return { name, makeTurn, score }
 }
 
-const player1 = {
-  symbol: 'X',
-  makeTurn: function (x) {
-    if (game.gameboard[x] !== '') {
-      gameControl.switchActivePlayer();
-      return;
-    } else game.gameboard[x] = 'X';
-    gameControl.currentChoice = x;
-    gameControl.player1Choices += x;
+const player1 = createPlayer('player1', 'X', 1);
+const player2 = createPlayer('player2', 'O', 2);
+
+// IIFE
+
+const game = (function () {
+
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+  const processForm = (e) => {
+    e.preventDefault();
+    player1.name = document.querySelector('#firstp').value;
+    player2.name = document.querySelector('#secondp').value;
+
+    displayControl.renderGrid();
+    displayControl.showInfo();
+    const form = document.querySelector('.form');
+    form.remove();
   }
-};
 
-const player2 = {
-  symbol: 'O',
-  makeTurn: function (x) {
-    if (game.gameboard[x] !== '') {
-      gameControl.switchActivePlayer();
-      return;
-    } else game.gameboard[x] = 'O';
-    gameControl.currentChoice = x;
-    gameControl.player2Choices += x;
+  const nextRound = function () {
+    document.querySelector('.container').innerHTML = '';
+    gameboard.fill('');
+    console.log(gameboard);
+    gameControl.activePlayer = player1;
+    gameControl.currentChoice = '';
+    gameControl.player1Choices = '';
+    gameControl.player2Choices = '';
+    gameControl.gameOver = false;
+    gameControl.tie = false;
+    document.querySelector('.players').textContent = '';
+    displayControl.renderGrid();
+    displayControl.showInfo();
+
+    document.querySelector('.container').style.pointerEvents = 'initial';
   }
-};
 
-const gameControl = {
-  activePlayer: 'player1',
-  currentChoice: '',
-  player1Choices: '',
-  player2Choices: '',
-  gameOver: false,
+  const restartGame = function () {
+    location.reload();
+  }
 
-  // checkPlayerChoice: function () {
-  //   if (game.gameboard[this.currentChoice] === 'X' ||
-  //     game.gameboard[this.currentChoice] === 'O'
-  //   ) {
-  //     this.gameTurn(this.currentChoice);
-  //     this.switchActivePlayer();
-  //   }
-  //   else {
-  //     this.activePlayer === 'player1' ? game.gameboard[this.currentChoice] = player1.symbol :
-  //       game.gameboard[this.currentChoice] = player2.symbol
-  //   }
+  return { gameboard, processForm, nextRound, restartGame }
+})();
 
-  // },
+const gameControl = (function () {
+  let activePlayer = player1;
+  let currentChoice = '';
+  let player1Choices = '';
+  let player2Choices = '';
+  let gameOver = false;
+  let tie = false;
 
-  switchActivePlayer: function () {
-    this.activePlayer === 'player1' ? this.activePlayer = 'player2' : this.activePlayer = 'player1';
-  },
-
-  gameTurn: function (x) {
-    this.activePlayer === 'player1' ? player1.makeTurn(x) : player2.makeTurn(x);
-    // gameControl.checkPlayerChoice();
+  const switchActivePlayer = function () {
+    this.activePlayer === player1 ? this.activePlayer = player2 : this.activePlayer = player1;
+  }
+  const gameTurn = function (x) {
+    this.activePlayer === player1 ? player1.makeTurn(x) : player2.makeTurn(x);
     gameControl.checkForWin();
     gameControl.checkForTie();
-    // game.logBoard();
     displayControl.updateGrid();
+    displayControl.showInfo();
     this.switchActivePlayer();
-  },
-
-  checkForWin: function () {
-    let arr = (this.activePlayer === 'player1' ? this.player1Choices : this.player2Choices);
+  }
+  const checkForWin = function () {
+    let arr = (this.activePlayer === player1 ? this.player1Choices : this.player2Choices);
 
     const winCombinations = ['048', '246', '345', '012', '678', '036', '147', '258'];
     const intersection = winCombinations.find(combination => {
@@ -85,9 +88,7 @@ const gameControl = {
       let counter = 0;
       let comb = combination.split("");
 
-
       for (let i = 0; i < 3; i++) {
-
         if (arr.includes(comb[i])) {
           counter++;
         }
@@ -98,32 +99,28 @@ const gameControl = {
       } else return false;
     });
 
-
     if (intersection) {
       displayControl.crossLine(intersection);
-      this.alertWinner()
-    } else return;
-  },
-
-  checkForTie: function () {
-    if ((this.gameOver !== true) && !(game.gameboard.includes(''))) {
-      alert('It is a tie');
       this.gameOver = true;
+      this.activePlayer.score++;
+      displayControl.disableControls();
+    } else return;
+  };
+
+  const checkForTie = function () {
+    if ((this.gameOver !== true) && !(game.gameboard.includes(''))) {
+      this.gameOver = true;
+      this.tie = true;
+      displayControl.showInfo();
       displayControl.disableControls();
     }
-  },
-
-  alertWinner: function () {
-    this.gameOver = true;
-    alert(`${this.activePlayer} wins`);
-    displayControl.disableControls();
-
   }
-}
+  return { activePlayer, currentChoice, player1Choices, player2Choices, gameOver, tie, switchActivePlayer, gameTurn, checkForWin, checkForTie }
+})();
 
-const displayControl = {
+const displayControl = (function () {
 
-  renderGrid: function () {
+  const renderGrid = function () {
 
     for (let i = 0; i < 3; i++) {
       document.querySelector('#container').appendChild(document.createElement('div')).
@@ -131,7 +128,6 @@ const displayControl = {
     }
 
     const rows = document.querySelectorAll('.row');
-
     rows.forEach((row) => {
       for (let i = 0; i < 3; i++) {
         row.appendChild(document.createElement('div')).
@@ -141,7 +137,6 @@ const displayControl = {
 
     const squares = document.querySelectorAll('.square');
     let index = 0;
-
     squares.forEach(square => {
       square.setAttribute('data-index-number', index);
       index++;
@@ -149,21 +144,21 @@ const displayControl = {
         gameControl.gameTurn(e.target.dataset.indexNumber);
       })
     })
-  },
+  }
 
-  updateGrid: function () {
+  const updateGrid = function () {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
       square.textContent = game.gameboard[square.dataset.indexNumber];
     })
-  },
+  }
 
-  disableControls: function () {
+  const disableControls = function () {
     const container = document.querySelector('.container');
     container.style.pointerEvents = 'none';
-  },
+  }
 
-  crossLine: function (inter) {
+  const crossLine = function (inter) {
     let crossedLine = inter.split('');
     console.log(crossedLine);
     crossedLine.forEach(crossed => {
@@ -175,26 +170,30 @@ const displayControl = {
         }
       })
     })
-
-
   }
 
+  const showInfo = function () {
+    const info = document.querySelector('.info');
+    const players = document.querySelector('.players');
+
+    players.textContent = `Players: ${player1.name}, ${player2.name}`;
+
+    if (gameControl.gameOver === true && gameControl.tie === false) {
+      players.textContent = `${gameControl.activePlayer.name} wins. Score: ${player1.score} : ${player2.score}`
+    } else if (gameControl.gameOver === true && gameControl.tie === true) {
+      players.textContent = `It is a tie`;
+    }
+  }
+  return { renderGrid, updateGrid, disableControls, crossLine, showInfo }
 }
+)();
 
-window.addEventListener('DOMContentLoaded', displayControl.renderGrid)
 
-// Крестики-нолики - цель игры составить на игровом поле линию из трех символов по горизонтали, вертикали или диагонали.
-// Один игрок может ставить крестики, другой - нолики. Кто первый составил линию, тот победил. Возможна ничья если никто не победил и все свободные места на поле закончились. Поле состоит из 9 клеток.
+const formSubmitBtn = document.querySelector('#submit');
+const nextRound = document.querySelector('#next');
+const restartGame = document.querySelector('#restart');
 
-// Изначальная цель - построить логику игры. Отобразить игровое поле в консоли. Поле состоит из 9 клеток. Клетка может быть либо пустой, либо с крестиком, либо с ноликом.
-// В игру играют player1 и player2. Один из них ставит крестики, другой - нолики. Игроки ходят по очереди. Первым ходит player1. player1 выбирает клетку для хода. Игра должна проверить, не занята ли уже эта клетка.
-
-// После каждого сделанного хода игра должна проверять, не победил ли игрок который сделал ход. Игра проверит все ходы игрока и сравнит их с победными комбинациями.
-
-// Отображение игры
-// 1. Создать пустое поле для игры.
-// 2. Нажатие на клетку должно менять значение в массиве
-// 3. Поле должно перезагружаться и отображать значения из массива
-
-// Проверка победы
+formSubmitBtn.addEventListener('click', game.processForm);
+nextRound.addEventListener('click', game.nextRound);
+restartGame.addEventListener('click', game.restartGame);
 
